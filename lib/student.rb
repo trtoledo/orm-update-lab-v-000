16faf720a77fd716bdcfbc9ee93ae150adbbf49a
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-09 22:14:46 +0000

    initialize, .create_table

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,42 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+
+  end
+
+  def self.drop_table
+
+  end
+
+  def save
+
+  end
+
+  def self.create
+
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
