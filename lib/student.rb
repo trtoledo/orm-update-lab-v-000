00ea9506696844c650b658ce3522cf6a62558fc9
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-09 22:28:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,11 @@ class Student
   end
 
   def save
+    sql = <<-SQL 
+    INSERT INTO songs (name, grade) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
 
   end
 
