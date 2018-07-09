2f1d9347c88f56ef9c13386b3f547ca27d66f440
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-09 22:37:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,10 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
 
   end
 
